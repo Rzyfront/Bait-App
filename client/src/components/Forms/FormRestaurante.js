680641ffd92a0de6a412ed867c165929e73be0a2
Diana0617 <77765286+Diana0617@users.noreply.github.com>
2023-04-12 11:44:28 -0500

    feat(./reservas.jsx) Creacion de un Local

---
+++ client/src/components/Forms/FormRestaurante.js
@@ -0,0 +1,35 @@
+const FormRest =() => {
+    return (
+        <FormRest>
+            <div> 
+                <label>Ubicacion</label>
+                <input type="text"></input>
+            </div>
+            <div> 
+                <label>Nombre</label>
+                <input></input>
+            </div>
+            <div> 
+                <label>Images</label>
+                <input></input>
+            </div>
+            <div> 
+                <label>Agenda</label>
+                <input></input>
+            </div> 
+            <div> 
+                <label>Tomar Reserva</label>
+                <input></input>
+            </div>
+            <div> 
+                <label>email</label>
+                <input type="mail"></input>
+            </div>
+            <div> 
+                <label>telefono</label>
+                <input type="Number"></input>
+            </div>
+        </FormRest>
+    )
+}
+export default FormRest; 
