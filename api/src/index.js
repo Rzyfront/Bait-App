1c28bb77fbebf8b062f4fc22474376e93056143d
franco4457 <franco_forex@hotmail.com>
2023-04-12 20:38:40 -0300

    feat(/routes): add endpoint POST image

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')();
 const morgan = require('morgan');//eslint-disable-line
 const mainRouter = require('./routes/index');
+const bodyParser = require('body-parser');//eslint-disable-line
 
 express.use(morgan('dev'));
-express.use(require('express').json());
+express.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+express.use(bodyParser.json({ limit: '50mb' }));
 
 express.use(mainRouter);
 
--- api/src/routes/images.js
+++ api/src/routes/images.js
@@ -1,6 +1,8 @@
 const imageRoute = require('express').Router();
 const getImages = require('../controllers/images/getImages');
+const postImages = require('../controllers/images/postImages');
 
 imageRoute.get('/', getImages);
+imageRoute.post('/', postImages);
 
 module.exports = imageRoute;
