c432a7e19c0889dc52cf7388b830ca6194b45220
junior mojica <juniormojica26@gmail.com>
2023-04-12 11:17:06 -0500

    feat(models/Menu): create menu model

---
+++ api/src/models/Menu.js
@@ -0,0 +1,20 @@
+const {DataTypes} = require("sequelize")
+
+module.exports = (sequelize)=>{
+sequelize.define("Menu",{
+    id : {
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement :true,
+        allowNull:false
+    },
+    dishId:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: "Dish",
+            key: "id"
+        }
+    }
+});
+};
