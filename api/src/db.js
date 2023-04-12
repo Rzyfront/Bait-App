4140d14565bf86e3d9e1a99acc22c7b162705526
Yixsi <yixsi3103@gmail.com>
2023-04-12 09:16:11 -0500

    feat(api/db.js): update config

--- api/src/db.js
+++ api/src/db.js
@@ -1,12 +1,46 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
 
-const db = new Sequelize(process.env.DB_URL, {
-  logging: false,
-  native: false,
-});
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/baitdb`,
+  {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  },
+);
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+// Reads all the comments from Models, requires them and adds them to modelDefiners
+fs.readdirSync(path.join(__dirname, '/models'))
+  .filter(
+    (file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js',
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+  });
+
+// Injectamos la conexion (sequelize) a todos los modelos
+modelDefiners.forEach((model) => model(sequelize));
+// Capitalizamos los nombres de los modelos ie: product => Product
+const entries = Object.entries(sequelize.models);
+const capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+// En sequelize.models están todos los modelos importados como propiedades
+// Para relacionarlos hacemos un destructuring
+
+// Associations
 
 module.exports = {
-  db,
-  ...db.models,
+  ...sequelize.models,
+  conn: sequelize, // import connection { conn } = require('./db.js');
 };
