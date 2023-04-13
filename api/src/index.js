267f49026f0c460aa21634f1a8ef964b50e85260
franco4457 <franco_forex@hotmail.com>
2023-04-12 22:17:23 -0300

    feat(/src/index): update initalmiddlewares

--- api/src/index.js
+++ api/src/index.js
@@ -6,6 +6,13 @@ const bodyParser = require('body-parser');//eslint-disable-line
 express.use(morgan('dev'));
 express.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 express.use(bodyParser.json({ limit: '50mb' }));
+express.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 express.use(mainRouter);
 
