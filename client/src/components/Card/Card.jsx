08253ff743f679827c290733b74fd5877ae1ceea
Rzyfront <rzyfront@gmail.com>
2023-04-12 20:07:53 -0500

    feat (./Card.jsx - ./Cards.jsx) Cartas con el rating auto variable y su contenedor

---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { GoLocation } from "react-icons/go";
+import { Rating as RatingStar } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+import img from "../../assets/restaurante.jpg";
+import "./Card.css";
+
+function Card({ Location, Rating, Name }) {
+  return (
+    <div className="Card">
+      <img src={img} alt="restaurante" className="imgCard" />
+      <div className="infoCard">
+        <h2 className="placeName">{Name || "No name"}</h2>
+        <div className="RatingGroup">
+          <p className="Rating">Rating: </p>
+          <RatingStar readOnly style={{ maxWidth: 100 }} value={Rating} />
+        </div>
+        <div className="LocationGroup">
+          <GoLocation />
+          <p className="Location">{Location || "No location"}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
