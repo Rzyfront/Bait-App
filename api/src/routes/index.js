3d3fa620894278b47ec6621cc6506056af7e7a81
franco4457 <franco_forex@hotmail.com>
2023-04-13 19:08:20 -0300

    feat(/authFirebaes):add Firebase login

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,10 +2,12 @@ const mainRouter = require('express').Router();
 const imageRoute = require('./images');
 const localsRoute = require('./locals');
 const administratorRoute = require('./administrator');
+// const loginRoute = require('./login');
 
 mainRouter.use('/locals', localsRoute);
 mainRouter.use('/images', imageRoute);
 mainRouter.use('/administrator', administratorRoute);
+// mainRouter.use('/login', loginRoute);
 
 mainRouter.get('/', (req, res) => {
   res.send('Hello World!');
