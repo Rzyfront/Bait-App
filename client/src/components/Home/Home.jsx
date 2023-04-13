675930f82fa3331c940006176435e6c705bfaae1
juancamilo1478 <juancamilo1478@gmail.com>
2023-04-12 20:37:23 -0500

    feat(./navbar.jsx,home.jsx) Creacion del componente navbarl y home

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,6 @@
+import "./Home.css";
+import { BsFillSearchHeartFill } from "react-icons/bs";
+const Home = () => {
+  return <div className="home"></div>;
+};
+export default Home;
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./Navbar.css";
+import Search_home from "./Search_home/Search_home";
+import { AiOutlineUserAdd } from "react-icons/ai";
+import { FaUserCircle } from "react-icons/fa";
+const Navbar = () => {
+  const [user, setUser] = useState(true);
+  return (
+    <div className="all_navbar">
+      <div className="nav_ico">
+        <img
+          src="https://res.cloudinary.com/dirsusbyy/image/upload/v1681249225/s0oftekkem6okolepefh.jpg"
+          alt="icono"
+          className="ico"
+        />
+        <h2 className="titule">Baid</h2>
+      </div>
+      <div>
+        <Search_home />
+      </div>
+      {user === false ? (
+        <div className="nav_login">
+          <AiOutlineUserAdd />
+          <h4>Registrate</h4>
+        </div>
+      ) : (
+        <div className="nav_login">
+          <FaUserCircle />
+          Mi perfil
+        </div>
+      )}
+    </div>
+  );
+};
+export default Navbar;
