3c3d3b521b67d223981f919795edfe3acfb06d25
Yixsi <yixsi3103@gmail.com>
2023-04-13 11:57:06 -0500

    feat(reviews/cat): relation added

--- api/src/controllers/reviews/postReviews.js
+++ api/src/controllers/reviews/postReviews.js
@@ -1,13 +1,16 @@
-const { Review } = require('../../db');
+const { Review, Category } = require('../../db');
 
 module.exports = async (req, res) => {
   const {
-    title, comment, photoTicket, verified,
+    title, comment, photoTicket, verified, categories,
   } = req.body;
   try {
+    const newCats = await Category.bulkCreate(categories);
     const newReview = await Review.create({
       title, comment, photoTicket, verified,
     });
+    newReview.addCategories(newCats.map((cat) => cat.id));
+
     return res.status(201).json({ success: true, local: newReview });
   } catch (error) {
     return res.status(400).json({ message: error.message, success: false });
