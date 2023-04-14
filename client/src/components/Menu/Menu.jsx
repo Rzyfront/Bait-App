2b4ae765e3feaabb911fd79b79e295ed8777bf02
rzyfront <rzyfront@gmail.com>
2023-04-13 21:10:31 -0500

    Fix /Remove Dependences

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import "./Menu.css";
 
 function Menu() {
-  return <div className="Menu"></div>;
+  return (
+    <div className="Menu"></div>
+  );
 }
+
 
 export default Menu;
