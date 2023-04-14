63eca6013c826aaac3aa2d868cca3b8307a18bb5
Diana0617 <77765286+Diana0617@users.noreply.github.com>
2023-04-14 11:02:01 -0500

    act (./Forms/Locales) feat(./Forms/Reviews)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,14 @@
 import "./App.css";
-<<<<<<< HEAD
-import Landing from "./components/Landing/Landing.jsx"
-
-function App() {
-  return <Landing />; 
-=======
-// import Landing from "./components/Landing/Landing.jsx";
+import Landing from "./components/Landing/Landing.jsx";
 // import Home from "./components/Home/Home";
-import Profile from "./components/Profile/Profile";
+//import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
-    <Profile />
+   // <Profile />
     // <Home />
-    // <Landing/>
+     <Landing/>
   );
->>>>>>> 2b4ae765e3feaabb911fd79b79e295ed8777bf02
 }
 
 export default App;
