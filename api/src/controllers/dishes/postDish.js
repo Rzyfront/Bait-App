90a3cb4c72591ffb97ce3a03857871d5af5b606e
franco4457 <franco_forex@hotmail.com>
2023-04-13 22:49:40 -0300

    feat(/routes/dishes): add endpoints POST PUT DEL

---
+++ api/src/controllers/dishes/postDish.js
@@ -0,0 +1,17 @@
+const { Menu, Dish } = require('../../db');// eslint-disable-line
+
+module.exports = async (req, res) => {
+  try {
+    const {
+      name, type, ingredients, price, description,
+    } = req.body;
+    // const { menuId } = req.params;
+    // const menu = await Menu.findByPk(menuId);
+    const newDish = await Dish.create({
+      name, type, ingredients, price, description,
+    });
+    res.status(201).json({ success: true, dish: newDish });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,13 +5,14 @@ const userRoutes = require('./users');
 
 const reviewsRoute = require('./reviews');
 const administratorRoute = require('./administrator');
+const dishesRouter = require('./dishes');
 // const loginRoute = require('./login');
 
 mainRouter.use('/locals', localsRoute);
 mainRouter.use('/reviews', reviewsRoute);
 mainRouter.use('/images', imageRoute);
 mainRouter.use('/administrator', administratorRoute);
-
+mainRouter.use('/dishes', dishesRouter);
 mainRouter.use('/users', userRoutes);
 // mainRouter.use('/login', loginRoute);
 
