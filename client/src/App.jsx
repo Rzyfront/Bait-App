9a3d0522cd49fa995df61288e9ff2c333417a56d
juancamilo1478 <juancamilo1478@gmail.com>
2023-04-14 12:05:04 -0500

    feat(./Filter.jsx) Order A-Z Z-A

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
-import React from "react";
 import "./App.css";
-<<<<<<< HEAD
 // import Landing from "./components/Landing/Landing.jsx";
 import Home from "./components/Home/Home";
 // import Profile from "./components/Profile/Profile";
@@ -10,19 +8,6 @@ function App() {
     // <Profile />
     <Home />
     // <Landing/>
-=======
-import { Landing, Home, Profile, FormRest } from "./components/components.js";
-import { Routes, Route } from "react-router-dom";
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/createplace" element={<FormRest />} />
-      <Route exact path="/" element={<Landing />} />
-    </Routes>
->>>>>>> 6f568db4771994267e43a29e3f24f0e02f82721e
   );
 }
 
