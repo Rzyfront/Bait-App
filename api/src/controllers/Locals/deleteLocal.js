d2c7aa7d8bea6a9763e112a9a82e129116087d51
franco4457 <franco_forex@hotmail.com>
2023-04-14 13:10:35 -0300

    fix(/deleteLocal): fix TypeError

--- api/src/controllers/Locals/deleteLocal.js
+++ api/src/controllers/Locals/deleteLocal.js
@@ -2,7 +2,7 @@ const { Local } = require('../../db');
 
 module.exports = async (req, res) => {
   try {
-    const localDeleted = await Local.findeByPk(req.local);
+    const localDeleted = await Local.findByPk(req.local.id);
     await localDeleted.destroy();
     return res.status(201).json({ success: true, localDeleted });
   } catch (err) {
