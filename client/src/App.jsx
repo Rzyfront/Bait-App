d66859797a5cf09ada17de25ac8abefdfe2d65ef
juancamilo1478 <juancamilo1478@gmail.com>
2023-04-13 19:02:46 -0500

    feat(./pagination.jsx) add pagination andfooter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 // import Landing from "./components/Landing/Landing.jsx";
 // import Home from "./components/Home/Home";
 import Profile from "./components/Profile/Profile";
+
 function App() {
   return (
     <Profile />
