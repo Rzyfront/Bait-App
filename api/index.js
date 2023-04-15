af5a72ea0289873d89158a6b2d36d1fbff3ce0e1
franco4457 <franco_forex@hotmail.com>
2023-04-15 14:26:04 -0300

    fix(/index): change alter-force

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { db } = require('./src/db');
 
 const PORT = process.env.PORT ?? 3001;
 
-db.sync({ alter: true }).then(() => {
+db.sync({ force: true }).then(() => {
   express.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
   });
