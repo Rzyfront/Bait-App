b7375d99532d28820223b775d35e6303e4fe93d2
junior mojica <juniormojica26@gmail.com>
2023-04-16 11:43:11 -0500

    refactor(users/putInactiveUser.js): change the messages

--- api/src/controllers/users/putInactiveUser.js
+++ api/src/controllers/users/putInactiveUser.js
@@ -4,18 +4,18 @@ module.exports = async (req, res) => {
   try {
     const { userId } = req.params;
     if (!userId) {
-      res.status(404).send({ message: 'Usuario no encontrado' });
+      res.status(404).send({ message: 'User ID not found' });
       return;
     }
     const user = await User.findOne({ where: { id: userId } });
 
     if (!user) {
-      res.status(404).send({ message: 'Usuario no encontrado' });
+      res.status(404).send({ message: 'User Not found' });
     } else {
       await user.update({ isActive: false });
-      res.status(200).send({ message: 'Usuario desactivado correctamente' });
+      res.status(200).send({ message:`User ${userId} deactivared succesfully`});
     }
   } catch (error) {
-    res.status(400).send({ message: `Error al desactivar el usuario, ${error.message}` });
+    res.status(400).send({ message: `Error deactivaring the user  ${error.message}` });
   }
 };
