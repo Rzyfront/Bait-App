015e819acf20ff984600d00109457f2674404220
franco4457 <franco_forex@hotmail.com>
2023-04-17 20:25:57 -0300

    fix(/reviews): add images to the review list

--- api/src/controllers/reviews/getReviews.js
+++ api/src/controllers/reviews/getReviews.js
@@ -1,43 +1,19 @@
-const { Review, Local } = require('../../db');
+const { Review, Local, Image } = require('../../db');
 
 module.exports = async (req, res) => {
   const { localId } = req.params;
 
   try {
     const localReviews = await Local.findByPk(localId, {
-
-      include: {
+      include: [{
         model: Review,
-        where: { verified: true },
-      },
+        include: [{ model: Image, attributes: ['url'] }],
+      }],
     });
 
     if (!localReviews) {
       return res.status(404).json({ error: 'Not found' });
     }
-
-    // const ratings = {
-    //   food: review.food,
-    //   service: review.service,
-    //   environment: review.environment,
-    //   qaPrice: review.qaPrice,
-    // };
-    // const filteredRatings = {};
-
-    // let totalRating = 0;
-    // let ratingCount = 0;
-
-    // Object.entries(ratings).forEach(([key, value]) => {
-    //   if (value !== null || value > 0) {
-    //     filteredRatings[key] = value;
-    //     totalRating += value;
-    //     ratingCount += 1;
-    //   }
-    // });
-
-    // const averageRating = ratingCount ? totalRating / ratingCount : null;
-
-    // const reviewWithAvgRating = { ...review.toJSON(), averageRating };
 
     return res.status(200).json({ reviews: localReviews.Reviews, success: true });
   } catch (error) {
