6596657e4ca218795c35a5752e7b85aaa85942f7
juancamilo1478 <juancamilo1478@gmail.com>
2023-04-17 02:58:49 -0500

    fix resolvi unos errores

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import store from "./redux/store/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
-//axios default
+//axios defaul
 axios.defaults.baseURL = "http://localhost:3001";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
