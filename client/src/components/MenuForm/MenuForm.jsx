be6eca7ec6fa92bb44b81b5713ef6a0b1844c717
Yixsi <yixsi3103@gmail.com>
2023-04-20 21:14:36 -0500

    Started menu

---
+++ client/src/components/MenuForm/MenuForm.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+const MenuForm = () => {
+    const [ menu, setMenu ] = useState([]);
+};
+
+export default MenuForm;
