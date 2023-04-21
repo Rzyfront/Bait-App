2f331b7ed4f5a1af97e1e8d8af291f27fde5c65b
franco4457 <franco_forex@hotmail.com>
2023-04-20 22:10:50 -0300

    Feat(/admin): send email if review rejected

--- api/src/controllers/administrator/deleteReview.js
+++ api/src/controllers/administrator/deleteReview.js
@@ -1,12 +1,16 @@
-const { Review } = require('../../db');
+const { sendReviewRejected } = require('../../config/nodemailer/nodemailer-config');
+const { Review, User } = require('../../db');
 
 module.exports = async (req, res) => {
   try {
     const { reviewId } = req.params;
-    const deletedReview = await Review.findByPk(reviewId);
+    const deletedReview = await Review.findByPk(reviewId, {
+      include: [{ model: User }],
+    });
     if (!deletedReview) throw new Error('Review not found');
     deletedReview.destroy();
-    deletedReview.save();
+    await deletedReview.save();
+    sendReviewRejected(deletedReview.User.email, deletedReview.comment);
     return res.status(201).json({ success: true, deletedReview });
   } catch (err) {
     return res.status(400).json({ success: false, message: err.message });
