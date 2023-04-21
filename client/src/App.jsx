b6ba581e01ce782954eedf58c8eb31bbc992b8b3
Yixsi <yixsi3103@gmail.com>
2023-04-21 01:19:03 -0500

    Otro más

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-
+import { useEffect } from 'react';
 import './App.css';
 import {
   Landing,
@@ -10,30 +10,34 @@ import {
   DataTreatment,
   Dashboard
 } from './components/components.js';
-import MenuForm from './components/MenuForm/MenuForm';
-import DishForm from './components/MenuForm/DishForm/DishForm';
 import Mapdata from './components/Map/Map';
-
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { checkUser } from './redux/actions/actions';
 
-
 function App () {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  // login
+  useEffect(() => {
+    console.log(localStorage.getItem('token'));
+    if (user && localStorage.getItem('token') !== null) {
+      dispatch(checkUser());
+    }
+  }, []);
+
   return (
-    <div className='App animated-element'>
+    <div className="App animated-element">
       <Routes>
-        <Route path='/home/:id' element={<Home />} />
-        <Route path='/profile/:id' element={<Profile />} />
-        <Route path='/createplace' element={<Locales />} />
-        <Route path='/answers' element={<Answers />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/dataTreatment' element={<DataTreatment />} />
-        <Route path='/map' element={<Mapdata/>} />
-        <Route path='/dashboard' element={<Dashboard/>} />
-        <Route path='/menu/:localId' element={<MenuForm/>} />
-        <Route path='/dish/:menuId' element={DishForm} />
-        <Route exact path='/' element={<Landing />} />
+        <Route path="/home/:id" element={<Home />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/createplace" element={<Locales />} />
+        <Route path="/answers" element={<Answers />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dataTreatment" element={<DataTreatment />} />
+        <Route path='/map' element={<Mapdata />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route exact path="/" element={<Landing />} />
       </Routes>
     </div>
   );
