185d262dfb1664eaf82f207bd2c5939972fcb2f9
franco4457 <franco_forex@hotmail.com>
2023-04-21 20:17:58 -0300

    feat(/local): admin can update local

--- api/src/controllers/locals/putLocal.js
+++ api/src/controllers/locals/putLocal.js
@@ -2,13 +2,14 @@ const { Characteristic } = require('../../db');
 
 module.exports = async (req, res) => {
   const {
-    name, location, schedule, email, characteristics,
+    name, location, schedule, email, characteristics, lat, lng,
   } = req.body;
+  const { userId, role } = req;
   try {
-    if (req.userId !== req.local.UserId) throw new Error('The user is not the owner of the locale');
+    if (userId !== req.local.UserId && (role !== 'admin' || role !== 'superAdmin')) throw new Error('The user is not the owner of the locale');
     await Characteristic.update(characteristics, { where: { id: req.local.id } });
     const updateLocal = await req.local.update({
-      name, location, schedule, email,
+      name, location, schedule, email, lat, lng,
     });
     const local = {
       id: updateLocal.id,
