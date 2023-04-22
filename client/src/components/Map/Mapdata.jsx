10f458f2efaa4a6947d43058cd6f3fc3abbbf175
juancamilo1478 <juancamilo1478@gmail.com>
2023-04-22 17:14:49 -0500

    mapas

--- client/src/components/Map/Mapdata.jsx
+++ client/src/components/Map/Mapdata.jsx
@@ -1,6 +1,6 @@
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import myIcon from './Icono';
 import L from 'leaflet';
 // const MAPCENTERDEFAULT = [40.574215, -105.08333];
@@ -10,6 +10,7 @@ const GEOCODE_URL = 'https://geocode.arcgis.com/arcgis/rest/services/World/Geoco
 
 function Mapdata ({ Mapcenter, statemap, handleBoton, handlemapdatas }) {
   // data information
+
   const reverseGeoCoding = async (lng, lat) => {
     const data = await (await fetch(GEOCODE_URL + `${lng},${lat}`)).json();
     return data;
@@ -21,7 +22,7 @@ function Mapdata ({ Mapcenter, statemap, handleBoton, handlemapdatas }) {
       click: async (e) => {
         const { lat, lng } = e.latlng;
         const information = await reverseGeoCoding(lng, lat);
-
+        handlemapdatas(information);
         map.eachLayer((layer) => {
           if (layer instanceof L.Marker) {
             map.removeLayer(layer);
@@ -30,6 +31,7 @@ function Mapdata ({ Mapcenter, statemap, handleBoton, handlemapdatas }) {
 
         // Crea un nuevo marcador con las coordenadas obtenidas del evento click
         const marker = L.marker([lat, lng]).addTo(map).bindPopup(`Dirección: ${information.address.LongLabel}`);
+
         marker.openPopup();
       }
       // dblclick: (e) => {
