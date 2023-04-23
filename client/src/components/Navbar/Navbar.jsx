d32316b410aeaccbe5776e14ece75d532b306387
rzyfront <rzyfront@gmail.com>
2023-04-23 02:56:52 -0500

    style/ resposive full landing and navbar and searchbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import './Navbar.css';
 import BaitLogo from '../../assets/LogoBait.svg';
+import BaitLogoSmall from '../../assets/LogoBaitSmall.svg';
 import SearchHome from './SearchHome/SearchHome';
 import { Link, useLocation } from 'react-router-dom';
-import { Login, DropdownUser, ToggleMenuNavbar } from '../components';
+import { Login, DropdownUser } from '../components';
 import { FaUserCircle } from 'react-icons/fa';
-import { BiMenu, BiMenuAltRight } from 'react-icons/bi';
 import { useDispatch, useSelector } from 'react-redux';
 import { ResetUser } from '../../redux/actions/actions';
 const Navbar = () => {
@@ -14,7 +14,6 @@ const Navbar = () => {
   const dataUser = useSelector((state) => state.user);
   const [barra, setbarra] = useState(false);
   const [toogleLogin, setToggleLogin] = useState(false);
-  const [toggleMenuR, setToggleMenuR] = useState(false);
 
   const close = () => {
     localStorage.clear();
@@ -30,51 +29,42 @@ const Navbar = () => {
           alt="Bait"
           className="Logo"
         />
+        <img
+          src={BaitLogoSmall}
+          alt="Bait"
+          className="Logo-Small"
+        />
       </Link>
 
-        <SearchHome />
+      <SearchHome />
 
       <div className="UserGroup">
         {JSON.stringify(dataUser) === '{}'
           ? (
-          <div
-            className="nav_login LogInGroup"
-            onClick={() => {
-              setToggleLogin(true);
-            }}
-          >
-            <FaUserCircle className="UserIcon" />
-            <h4 className="LogIn"> Iniciar Sesion</h4>
-          </div>
+            <div
+              className="nav_login LogInGroup"
+              onClick={() => {
+                setToggleLogin(true);
+              }}
+            >
+              <FaUserCircle className="UserIcon" />
+              <h4 className="LogIn"> Iniciar Sesion</h4>
+            </div>
             )
           : (
-          <div className="nav_login" onClick={barra
-            ? () => {
-                setbarra(false);
-              }
-            : () => {
-                setbarra(true);
-              }}
-          >
-            <FaUserCircle />
-            {dataUser.user.name}
-            {barra &&
-            (<div className={`dropDown ${barra && 'scale-up-ver-top'}`}>
-              <DropdownUser close={close}/>
+            <div className="nav_login" onClick={barra
+              ? () => {
+                  setbarra(false);
+                }
+              : () => {
+                  setbarra(true);
+                }}
+            >
+              <FaUserCircle className="UserIcon" />
+              <h4 className='Name-User-bar'>{dataUser.user.name}</h4>
+              {barra && <DropdownUser close={close} />}
             </div>
-            )
-            }
-          </div>
             )}
-            <div className='toggleMenuNavbar'>
-             {toggleMenuR
-               ? (<>
-               <BiMenuAltRight className='toggleMenuNavbar-close' onClick={() => setToggleMenuR(false)}/>
-                  <ToggleMenuNavbar/>
-                  </>
-                 )
-               : <BiMenu className='toggleMenuNavbar-close' onClick={() => setToggleMenuR(true)}/>}
-            </div>
       </div>
     </div>
   );
