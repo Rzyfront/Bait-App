748cc231f1e256fe1c7c020f553e147c1158e8e0
franco4457 <franco_forex@hotmail.com>
2023-04-23 20:26:11 -0300

    feat(/nodemailer): add template email

--- api/src/controllers/administrator/deleteReview.js
+++ api/src/controllers/administrator/deleteReview.js
@@ -3,14 +3,21 @@ const { Review, User } = require('../../db');
 
 module.exports = async (req, res) => {
   try {
+    const { reason } = req.query;
     const { reviewId } = req.params;
     const deletedReview = await Review.findByPk(reviewId, {
       include: [{ model: User }],
     });
     if (!deletedReview) throw new Error('Review not found');
-    deletedReview.destroy();
+    await deletedReview.destroy();
     await deletedReview.save();
-    sendReviewRejected(deletedReview.User.email, deletedReview.comment);
+    sendReviewRejected(
+      deletedReview.User.email,
+      deletedReview.User.name,
+      deletedReview.title,
+      deletedReview.comment,
+      reason,
+    );
     return res.status(201).json({ success: true, deletedReview });
   } catch (err) {
     return res.status(400).json({ success: false, message: err.message });
--- api/src/controllers/users/verifyUser.js
+++ api/src/controllers/users/verifyUser.js
@@ -15,9 +15,7 @@ module.exports = async (req, res) => {
     userToVerify.verified = verified;
     await userToVerify.save();
 
-    res.status(200).send(`<h1>User verified</h1>
-    <a href='http://localhost:5173/'>Inica sesión en Bait</a>
-    `);
+    res.status(200).send(`<html lang="es"><head><meta charset="UTF-8"><title>Confirmación de correo electrónico</title><style>body{font-family:Arial,sans-serif;font-size:16px;line-height:1.5;color:#333;height:100vh;display:grid;place-content:center}.container{max-width:600px;margin:0 auto;padding:1.5rem;background-color:#f9f9f9;border:1px solid #ddd;text-align:center}h1{font-size:24px;margin:0 0 1.5rem}p{margin:0 0 1.5rem}.button{display:inline-block;background-color:#007bff;color:#fff;padding:1rem 2rem;text-decoration:none;border-radius:5px;margin-top:1.5rem}</style></head><body><div class="container"><h1>¡Gracias por verificar tu correo electrónico!</h1><p>Tu correo electrónico ha sido verificado con éxito. Ahora puedes disfrutar de todas las funcionalidades de nuestro sitio web.</p><a href="${process.env.FRONT_DEPLOY}" class="button">Ir a la página principal</a></div></body></html>`);
   } catch (error) {
     res.status(400).send('<h1>User not verified</h1>');
   }
