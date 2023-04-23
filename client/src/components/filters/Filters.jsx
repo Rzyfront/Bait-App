9d2c8b3f18ab841c7727d1dd46a13e8b3c1ca4ca
DianaQuintana <dcqy.95@gmail.com>
2023-04-23 18:31:45 -0500

    filters

--- client/src/components/filters/Filters.jsx
+++ client/src/components/filters/Filters.jsx
@@ -1,19 +1,29 @@
 import './Filters.css';
 import { MdAddBusiness } from 'react-icons/md';
 import { RiRefreshFill } from 'react-icons/ri';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import Select from 'react-select';
 // import { TbToolsKitchen2 } from "react-icons/tb";
-import { useDispatch, useSelector } from 'react-redux';
-import { order, reset } from '../../redux/actions/actions';
-import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { searchByFilters } from '../../redux/actions/cards';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 // import Filtertype from "./filtertype/Filtertype";
 const Filters = () => {
-  const [filterState, setFilterState] = useState(false);
-  const [selectOrder, setSelectOrder] = useState('');
+  const location = useLocation();
   const dispatch = useDispatch();
-  const ContainerCards = useSelector((state) => state.cards);
+  const navigate = useNavigate();
+  const filterState = {
+    specialty: '',
+    characteristics: [],
+    rating: '',
+    alphabet: ''
+  };
+
+  const [filters, setFilters] = useState(filterState);
   const [selectedOptions, setSelectedOptions] = useState([]);
+  const [special, setSpecial] = useState([]);
 
   const Caracteristicaslist = [
     { value: 'wifi', label: 'Wifi' },
@@ -28,70 +38,60 @@ const Filters = () => {
     { value: 'romantic', label: 'Romantico' }
   ];
 
-  const handlecafication = () => {
-    if (selectOrder !== '') {
-      setSelectOrder('');
-    }
-    if (filterState === false) {
-      dispatch(order(ContainerCards, 'best'));
-      setFilterState(true);
-    } else {
-      dispatch(reset());
-      setFilterState(false);
-    }
+  useEffect(() => {
+    axios.get('/locals/specialties')
+      .then(res => setSpecial(res.data.allSpecialties.map(e => e.specialty)))
+      .catch(err => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    dispatch(searchByFilters(filters));
+  }, [filters]);
+
+  const handleFilters = (e) => {
+    const { name, value } = e.target;
+    if (name === 'rating') {
+      setFilters({ ...filters, alphabet: '', [name]: value });
+    } else if (name === 'alphabet') {
+      setFilters({ ...filters, rating: '', [name]: value });
+    } else { setFilters({ ...filters, [name]: value }); };
+    navigate('/home/1');
   };
 
-  const handleSelect = (e) => {
-    if (filterState === true) {
-      handlecafication();
-    }
-    console.log(e.target.value);
-    setSelectOrder(e.target.value);
-    dispatch(order(ContainerCards, e.target.value));
+  const onRefresh = () => {
+    navigate('/home/1');
+    console.log(filters);
+    setFilters(filterState);
   };
 
   const handleMultiSelectChange = (selectedOptions) => {
     setSelectedOptions(selectedOptions);
-    console.log(selectedOptions);
+    setFilters({ ...filters, characteristics: selectedOptions.map(e => e.value) });
   };
 
   return (
     <div className="Filters">
       <div className='Left-Home-Buttons'>
         <Link to="/createplace">
-        <div className="AddPlace">
-          <h2 className="AddPlace_Text">Inscribe tu sitio</h2> <MdAddBusiness />
-        </div>
-      </Link>
-      <Link to="/home/1?name=&city=">
-        <div className="ResetHome">
+          <div className="AddPlace">
+            <h2 className="AddPlace_Text">Inscribe tu sitio</h2> <MdAddBusiness />
+          </div>
+        </Link>
+        <div className="ResetHome" onClick={onRefresh}>
           <RiRefreshFill />
         </div>
-      </Link>
       </div>
 
       <div className="FiltersGroup">
-        {/* <div
-          className={filterState === false ? 'FilterOff' : 'FilterOn'}
-          onClick={handlecafication}
-        >
-          <h3>Mejor calificacion</h3>
-        </div> */}
+
         <select
+          name = "specialty"
           className="Restriction"
-          onChange={handleSelect}
-          value={selectOrder}
+          onChange={handleFilters}
+          value={filters.specialty}
         >
-          <option value="" disabled>
-            Tipo de comida
-          </option>
-          <option value={'mayor'}>Vegana</option>
-          <option value={'menor'}>Libre de gluten</option>
-          <option value={'mayor'}>Keto diet</option>
-          <option value={'mayor'}>Mexicana</option>
-          <option value={'mayor'}>Argentina</option>
-          <option value={'mayor'}>Italiana</option>
-          <option value={'mayor'}>Peruana</option>
+          <option value="" disabled> Tipo de comida </option>
+          {special?.map((spe, i) => <option key={i} value={spe}>{spe}</option>)}
         </select>
 
         <Select
@@ -99,59 +99,34 @@ const Filters = () => {
           onChange={handleMultiSelectChange}
           options={Caracteristicaslist}
           isMulti
-        />
-        {/* <select
-          className="Caracteristics"
-          onChange={handleSelect}
-          value={selectOrder}
-        >
-          <option value="" disabled>
-            Caracteristicas adicionales
-          </option>
-          <option value={'mayor'}>Pet frenly</option>
-          <option value={'menor'}>Wifi</option>
-          <option value={'menor'}>Parqueadero</option>
-          <option value={'menor'}>Musica</option>
-          <option value={'menor'}>Romantico</option>
-          <option value={'menor'}>Familiar</option>
-          <option value={'menor'}>Exteriores</option>
-        </select> */}
-        <select
-          className="RatingOrder"
-          onChange={handleSelect}
-          value={selectOrder}
-        >
-          <option value="" disabled>
-            Ordena por Rating
-          </option>
-          <option value={'mayor'}>Mayor rating</option>
-          <option value={'menor'}>Menor rating</option>
-        </select>
-        {/* <div
-        className="filter_calification"
-        onClick={() => handlecalification("opentype")}
-      >
-        <TbToolsKitchen2 /> <h3>Cocina</h3>
-        {filterState.opentype === false ? (
-          <div></div>
-        ) : (
-          <div className="filter_type">
-            <Filtertype />
-          </div>
-        )}
-      </div> */}
+  />
 
         <select
-          className="AlphaOrder"
-          onChange={handleSelect}
-          value={selectOrder}
+          name = "rating"
+          className="RatingOrder"
+          onChange={handleFilters}
+          value={filters.rating}
         >
-          <option value="" disabled>
+          <option value="" >
+            Ordena por Rating
+          </option>
+          <option value={'ratingASC'}>Mayor rating</option>
+          <option value={'ratingDESC'}>Menor rating</option>
+        </select>
+
+        <select
+          name = "alphabet"
+          className="AlphaOrder"
+          onChange={handleFilters}
+          value={filters.alphabet}
+        >
+          <option value="">
             Ordena Alfabeticamente
           </option>
-          <option value={'A-Z'}>A-Z</option>
-          <option value={'Z-A'}>Z-A</option>
+          <option value={'nameASC'}>A-Z</option>
+          <option value={'nameDESC'}>Z-A</option>
         </select>
+
       </div>
     </div>
   );
--- client/src/redux/actions/cards.js
+++ client/src/redux/actions/cards.js
@@ -1,11 +1,42 @@
 import axios from 'axios';
 export const SEARCH_BY_QUERY = 'SEARCH_BY_QUERY';
+export const SEARCH_BY_FILTERS = 'SEARCH_BY_FILTERS';
+
 export const searchByQuery = (name, city) => {
   return async (dispatch) => {
     try {
       const response = await axios.get(`/locals?name=${name}&location=${city}`);
       const info = response.data;
       return dispatch({ type: SEARCH_BY_QUERY, payload: info });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+};
+
+export const searchByFilters = (filter) => {
+  const {
+    name = '',
+    city = '',
+    specialty,
+    characteristics,
+    rating,
+    alphabet,
+    page
+  } = filter;
+  return async (dispatch) => {
+    try {
+      const character = {};
+      if (characteristics.length) {
+        for (const element of characteristics) {
+          character[element] = true;
+        }
+      };
+      const response = await axios.get(
+        `/locals/page/${page ?? 1}?name=${name}&location=${city}&specialty=${specialty}&order=${rating || alphabet}&characteristics=${JSON.stringify(character)}`);
+      const info = response.data;
+      info.filters = filter;
+      return dispatch({ type: SEARCH_BY_FILTERS, payload: info });
     } catch (error) {
       console.log(error.message);
     }
