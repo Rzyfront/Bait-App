1f50a5e691ad1e716cfaf3cd87806a9e4544c25f
juancamilo1478 <juancamilo1478@gmail.com>
2023-04-23 22:20:02 -0500

    cambios mi rama

---
+++ client/src/components/Locales/ErrorsDatabasic.js
@@ -0,0 +1,18 @@
+const regGmail = /^\S+@\S+\.\S+$/;
+
+export const ErrorsDatabasic = (contenido) => {
+  const error = {};
+  if (contenido.name.length > 30) {
+    error.name = 'Nombre muy grande';
+  }
+  if (!contenido.specialty.length) {
+    error.specialty = 'Escoje una especialidad';
+  }
+  if (!regGmail.test(contenido.email)) {
+    error.email = 'Correo no valido';
+  }
+  if (JSON.stringify(contenido.location) === '{}') {
+    error.location = 'selecciona punto';
+  }
+  return error;
+};
--- client/src/components/Locales/Locales.jsx
+++ client/src/components/Locales/Locales.jsx
@@ -7,19 +7,66 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useUploadImage } from '../../hooks/useUploadImage';
 import { Loading } from '@nextui-org/react';
 import { useDispatch, useSelector } from 'react-redux';
-import { validateForm } from './localHelpers';
 import TYC from './TYC';
-import Chars from './Chars/Chars';
-import DataLocal from './DataLocal/DataLocal';
+import DatabasicLocal from './DataLocal/DatabasicLocal';
+import Mapdata from '../Map/Mapdata';
+import SearchMap from '../Map/SearchMap/Searchmap';
 import { createLocal } from '../../redux/actions/local';
+import { ErrorsDatabasic } from './ErrorsDatabasic';
+import Chars from './Chars/Chars';
+
 function Locales () {
+  const ubication = useSelector((state) => state.ubication);
+  const positionMap = useSelector((state) => state.ubication);
+  const [Mapcenter, setMapcenter] = useState([40.574215, -105.08333]);
+  /// /
   const Navigate = useNavigate();
   const { image, loading, handleChangeimage } = useUploadImage();
-  const { success, error } = useSelector(state => state);
   const dispatch = useDispatch();
   const [termsAndConditions, setTemsAndConditions] = useState(true);
+
+  // map controllers
+  useEffect(() => {
+    if (Mapcenter[0] !== positionMap.lat && Mapcenter[1] !== positionMap.lng) {
+      setMapcenter([positionMap.lat, positionMap.lng]);
+    }
+  }, [positionMap]);
+
+  const [statemap, setStatemap] = useState(false);
+  const [mapSearch, setMapsearch] = useState('');
+  const handleMap = (e) => {
+    setMapsearch(e.target.value);
+  };
+  const handleBoton = () => {
+    setStatemap(false);
+  };
+  const searchCity = async () => {
+    const data = await SearchMap(mapSearch);
+    if (data) {
+      setMapcenter(data);
+      setStatemap(true);
+    } else {
+      toast.error('No existe esta ciudad', {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 2000
+      });
+    }
+  };
+  const handlemapdatas = (information) => {
+    const data = {
+      lat: information.location.y,
+      lng: information.location.x,
+      location: information.address
+        .LongLabel
+    };
+    setInputs({
+      ...inputs,
+      location: data
+    });
+  };
+  /// inputs and erros
   const [inputs, setInputs] = useState({
-    location: '',
+    location: {},
     name: '',
     images: [],
     email: '',
@@ -27,10 +74,14 @@ function Locales () {
     schedule: '',
     specialty: ''
   });
+  // Error controller
   const [errors, setErrors] = useState({
-
+    location: '',
+    name: '',
+    email: '',
+    schedule: '',
+    specialty: ''
   });
-
   const [chekinputs, setChekInputs] = useState({
     wifi: false,
     parking_lot: false,
@@ -43,6 +94,13 @@ function Locales () {
     family_style: false,
     romantic: false
   });
+  useEffect(() => {
+    setErrors(
+      ErrorsDatabasic({
+        ...inputs
+      })
+    );
+  }, [inputs]);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -50,43 +108,25 @@ function Locales () {
       ...inputs,
       [name]: value
     });
-
-    setErrors(
-      validateForm({
-        ...inputs,
-        [name]: value
-      })
-    );
   };
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(errors);
     if (!Object.values(errors).length) {
-      dispatch(createLocal(inputs));
-      setTimeout(() => {
-        Navigate('/home/1?name=&city=');
-      }, 3000);
-      // setInputs({
-      //   location: '',
-      //   name: '',
-      //   images: '',
-      //   email: '',
-      //   phone: '',
-      //   schedule: '',
-      //   specialty: ''
-      // });
-      // setErrors({
-      // });
-      // setChekInputs({
-      //   wifi: false,
-      //   parking_lot: false,
-      //   outdoor_seating: false,
-      //   live_music: false,
-      //   table_service: false,
-      //   big_group: false,
-      //   work_friendly: false,
-      //   pet_friendly: false
-      // });
+      const response = await dispatch(createLocal(inputs));
+      if (response === true) {
+        toast.success('¡Local creado satisfactoriamente!', {
+          position: toast.POSITION.TOP_CENTER,
+          autoClose: 2000
+        });
+        setTimeout(() => {
+          Navigate(`/home/1?name=&city=${ubication.city}`);
+        }, 2000);
+      }
+    } else {
+      toast.error('Faltan datos', {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 2000
+      });
     }
   };
 
@@ -100,18 +140,6 @@ function Locales () {
       ...inputs,
       [name]: value
     });
-    setErrors(
-      validateForm({
-        ...inputs,
-        [name]: value
-      })
-    );
-  };
-
-  const handleCheck = (e) => {
-    console.log(e.target.value);
-    const { name } = e.target;
-    setChekInputs({ ...chekinputs, [name]: true });
   };
 
   function handleClick () {
@@ -127,88 +155,86 @@ function Locales () {
       setInputs({ ...inputs, images: data });
 
       setErrors(
-        validateForm({
+        ErrorsDatabasic({
           ...inputs,
           images: [data]
         })
       );
     }
   }, [image]);
-
-  success && toast.success('¡Local creado satisfactoriamente!', {
-    position: toast.POSITION.TOP_CENTER,
-    autoClose: 1000
-  });
-
-  error && toast.error('Falló al crear el local', {
-    position: toast.POSITION.TOP_CENTER,
-    autoClose: 1000
-  });
-
+  const handleCheck = (e) => {
+    const { name } = e.target;
+    setChekInputs({ ...chekinputs, [name]: true });
+  };
   return (
     <div className='Create-Locals-Form animated-element'>
-      { termsAndConditions
-        ? <TYC src={BaitLogo} handleClick={handleClick}/>
+      {termsAndConditions
+        ? <TYC src={BaitLogo} handleClick={handleClick} />
         : <div className='locales_data animated-element'>
-        <Link to='/home/1?name=&city=' className='LinkLogo'>
-          <img
-            src={BaitLogo}
-            alt='Bait'
-            className='Logo'
-            width='60px'
-            height='60px'
-          />
-        </Link>
-        <h1>Crea un nuevo Local</h1>
-        <form onSubmit={handleSubmit}>
-          <DataLocal
-             handleChange={handleChange}
-             inputs={inputs}
-             errors={errors}
-             handleSelect={handleSelect}
-          />
-          <label className='imagen' htmlFor='imagen'>
-            Imágenes
-          </label>
-          <input
-            type='file'
-            name='imagen'
-            accept='image/png,image/jpeg,image/jpg,image/gif'
-            // multiple
-            onChange={handleChangeimages}
-          ></input>
-
-          {image.length
-            ? (
-                image.map((image, i) => (
-              <img
-                key={i}
-                src={image.url}
-                alt='imagen'
-                className='LocalesImage'
-              />
-                ))
-              )
-            : loading === true
-              ? (
-            <Loading color="primary"/>
-                )
-              : (
+          <Link to='/home/1?name=&city=' className='LinkLogo'>
             <img
-              src='https://res.cloudinary.com/dirsusbyy/image/upload/v1680389194/ppex43qn0ykjyejn1amk.png'
-              alt='image default'
-              className='LocalesImage'
+              src={BaitLogo}
+              alt='Bait'
+              className='Logo'
+              width='60px'
+              height='60px'
             />
-                )}
+          </Link>
+          <h1>Crea un nuevo Local</h1>
+          <form onSubmit={handleSubmit}>
+            <DatabasicLocal
+              handleChange={handleChange}
+              inputs={inputs}
+              errors={errors}
+              handleSelect={handleSelect}
+              searchCity={searchCity}
+              setMapsearch={setMapsearch}
+              handleMap={handleMap}
+            />
+            <div className='MapSize'>
+              <Mapdata Mapcenter={Mapcenter} statemap={statemap} handleBoton={handleBoton} handlemapdatas={handlemapdatas} />
+            </div>
 
-          <Chars handleCheck = {handleCheck} chekinputs = {chekinputs}/>
+            <label className='imagen' htmlFor='imagen'>
+              Imágenes
+            </label>
+            <input
+              type='file'
+              name='imagen'
+              accept='image/png,image/jpeg,image/jpg,image/gif'
+              // multiple
+              onChange={handleChangeimages}
+            ></input>
 
-          <button type='submit' className='Send-Locals'> ENVIAR</button>
-          <ToastContainer theme='colored'/>
-        </form>
-      </div>}
+            {image.length
+              ? (
+                  image.map((image, i) => (
+                  <img
+                    key={i}
+                    src={image.url}
+                    alt='imagen'
+                    className='LocalesImage'
+                  />
+                  ))
+                )
+              : loading === true
+                ? (
+                  <Loading color="primary" />
+                  )
+                : (
+                  <img
+                    src='https://res.cloudinary.com/dirsusbyy/image/upload/v1680389194/ppex43qn0ykjyejn1amk.png'
+                    alt='image default'
+                    className='LocalesImage'
+                  />
+                  )}
+
+            <Chars handleCheck={handleCheck} chekinputs={chekinputs} />
+            <button type='submit' className='Send-Locals'> ENVIAR</button>
+            <ToastContainer theme='colored' />
+          </form>
+        </div>}
     </div>
   );
 }
-
 export default Locales;
