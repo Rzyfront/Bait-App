1c341a8cf847807f83ff02af501ff63365cdeca1
franco4457 <franco_forex@hotmail.com>
2023-04-26 22:01:47 -0300

    feat docs

--- api/src/controllers/administrator/patchSupendUser.js
+++ api/src/controllers/administrator/patchSupendUser.js
@@ -11,3 +11,55 @@ module.exports = async (req, res) => {
     res.status(400).json({ success: false, message: error.message });
   }
 };
+/**
+ *
+ * @swagger
+ * /administator/suspend/{userId}:
+ *     patch:
+ *       summary: Suspender un usuario
+ *       description: Este endpoint suspende a un usuario en particular.
+ *       tags: [Administrador]
+ *       parameters:
+ *         - in: header
+ *           name: Authorization
+ *           schema:
+ *             type: string
+ *           required: true
+ *           description: Token de autenticación JWT.
+ *         - name: userId
+ *           in: path
+ *           description: ID del usuario a suspender
+ *           required: true
+ *           schema:
+ *             type: integer
+ *       security:
+ *         - bearerAuth: []
+ *       responses:
+ *         '201':
+ *           description: OK
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   success:
+ *                     type: boolean
+ *                     description: Indica si la operación se realizó con éxito.
+ *                     example: true
+ *         '400':
+ *           description: Error
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   success:
+ *                     type: boolean
+ *                     description: Indica si la operación falló.
+ *                     example: false
+ *                   message:
+ *                     type: string
+ *                     description: Mensaje de error detallado.
+ *                     example: error
+ *
+ * */
