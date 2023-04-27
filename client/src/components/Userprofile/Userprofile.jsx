60976ab78581ce29d977806b5937c21022beef33
junior mojica <juniormojica26@gmail.com>
2023-04-27 00:06:55 -0500

    feat(component/userProfile):renderizado condicional dependiendo el tipo de usuario

--- client/src/components/Userprofile/Userprofile.jsx
+++ client/src/components/Userprofile/Userprofile.jsx
@@ -53,157 +53,296 @@ function Userprofile () {
   
 
   userProfile && console.log(userProfile.user?.Reviews);
-  user && console.log(user);
+  user && console.log(user.role);
 
-  console.log(profileImg);
+ 
   
   const handleChangeimages = (event) => {
     handleChangeimage(event);
   };
-  
-  return (
-    <div className='userProfileContainer'>
-      {openInfoModal ? <InfoModal
-       closeModal={setOpenInfoModal}
-       name ={user.name}
-       lastname ={user.lastname}
-       age ={user.age}
-       email={user.email}
-        phone_number ={user.phone_number}
-        location ={user.location}
-        verified={user.verified}
+    return(
+    (user?.role === "user"
+      ? <div className='userProfileContainer'>
+        {openInfoModal ? <InfoModal
+          closeModal={setOpenInfoModal}
+          name={user.name}
+          lastname={user.lastname}
+          age={user.age}
+          email={user.email}
+          phone_number={user.phone_number}
+          location={user.location}
+          verified={user.verified}
         /> : null}
 
-      {openBonoModal ? <BonoModal
-        closeBonoModal={setOpenBonoModal}
-        name={user.name}
-      /> : null}
-    <Navbar />
-    <div className='infoSection'>
-        {/* <h2 className='userProfileText'>perfil de usuario</h2> */}
-        {user && (
-          <div className="userInfo">
-            <div className="Decorator"></div>
-            <div className="Info">
-              {profileImg.length
-                ? (
-                  image.map((image, i) => (
-                    <img
-                      key={i}
-                      src={profileImg}
-                      alt='imagen'
-                      className='userImage'
-                    />
-                  ))
-                )
-                : loading === true
+        {openBonoModal ? <BonoModal
+          closeBonoModal={setOpenBonoModal}
+          name={user.name}
+        /> : null}
+      
+        <div className='infoSection'>
+          {/* <h2 className='userProfileText'>perfil de usuario</h2> */}
+          {user && (
+            <div className="userInfo">
+              <div className="Decorator"></div>
+              <div className="Info">
+                {profileImg.length
                   ? (
-                    <Loading color="primary" />
+                    image.map((image, i) => (
+                      <img
+                        key={i}
+                        src={profileImg}
+                        alt='imagen'
+                        className='userImage'
+                      />
+                    ))
                   )
-                  : (
-                    <img
-                      src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png'
-                      alt='image default'
-                      className='userImage'
-                    />
-                  )}
-              <h2>{user.name + ' ' + user.lastname}</h2>
-              
-             
+                  : loading === true
+                    ? (
+                      <Loading color="primary" />
+                    )
+                    : (
+                      <img
+                        src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png'
+                        alt='image default'
+                        className='userImage'
+                      />
+                    )}
+                <h2>{user.name + ' ' + user.lastname}</h2>
+
+
+              </div>
+
             </div>
-            
-          </div>
-        )}
-        <div className='userButtonContainer'>
-          <button className='userButtons'
-           onClick={()=>{setOpenInfoModal(!openInfoModal)}}>
-            Informacion
-           </button>
-
-          <button 
-
-            className={`userButtons ${scroll && "scroll" }`}   >
-          
-            Reviews
-          </button>
-
-          <button 
-          className='userButtons' 
-          onClick={() => {setOpenBonoModal(!openBonoModal) }}>
-            Bonificaciones
+          )}
+          <div className='userButtonContainer'>
+            <button className='userButtons'
+              onClick={() => { setOpenInfoModal(!openInfoModal) }}>
+              Informacion
             </button>
-          
-          
+
+            <button
+
+              className={`userButtons ${scroll && "scroll"}`}   >
+
+              Reviews
+            </button>
+
+            <button
+              className='userButtons'
+              onClick={() => { setOpenBonoModal(!openBonoModal) }}>
+              Bonificaciones
+            </button>
+
+
+          </div>
         </div>
-    </div>
-    
-   
-   
-     
-    <div className='userAvatarContainer'>
-     
-      <p>Cambiar Imagen De Perfil</p>
-        <input
-          type='file'
-          name='imagen'
-          accept='image/png,image/jpeg,image/jpg,image/gif'
-          onChange={handleChangeimages}
-          title='Cambiar Avatar'
-        ></input>
-    </div>
-      
-      
-     
 
-      <div className='userReviews'>
-        <h1 className='reviewTittle'>Reviews </h1>
-        {reviews && reviews.map((review) => {
-          return (
-            <div className='mainContainer'>
-              <div key={review.id} className='reviewContainer'>
-                <div className='reviewTitle'>
-                  <h3>Titulo: {review.title}</h3>
-                </div>
 
-              <div className='reviewInfoContainer'>
 
-                  <div className='reviewCalification'>
-                    <p>Comentario: {review.comment}</p>
-                    <p>Calificaciones:</p>
-                    <p>Food :{review.food}</p>
-                    <p>Service :{review.service}</p>
-                    <p>Environment :{review.environment}</p>
+
+        <div className='userAvatarContainer'>
+
+          <p>Cambiar Imagen De Perfil</p>
+          <input
+            type='file'
+            name='imagen'
+            accept='image/png,image/jpeg,image/jpg,image/gif'
+            onChange={handleChangeimages}
+            title='Cambiar Avatar'
+          ></input>
+        </div>
+
+
+
+
+        <div className='userReviews'>
+          <h1 className='reviewTittle'>Reviews </h1>
+          {reviews && reviews.map((review) => {
+            return (
+              <div className='mainContainer'>
+                <div key={review.id} className='reviewContainer'>
+                  <div className='reviewTitle'>
+                    <h3>Titulo: {review.title}</h3>
                   </div>
 
-                  <figure className='imgContainer'>
-                    <img src={review.Image?.url} alt="" />
-                  </figure>
+                  <div className='reviewInfoContainer'>
 
-                
+                    <div className='reviewCalification'>
+                      <p>Comentario: {review.comment}</p>
+                      <p>Calificaciones:</p>
+                      <p>Food :{review.food}</p>
+                      <p>Service :{review.service}</p>
+                      <p>Environment :{review.environment}</p>
+                    </div>
+
+                    <figure className='imgContainer'>
+                      <img src={review.Image?.url} alt="" />
+                    </figure>
+
+
+
+                  </div>
+
+
+                  <div className='reviewButtons'>
+                    <button>Modificar</button>
+                    <button>Eliminar</button>
+
+                  </div>
+                </div>
 
               </div>
-                
-               
-                <div className='reviewButtons'>
-                  <button>Modificar</button>
-                  <button>Eliminar</button>
+
+            )
+          })}
+        </div>
+
+      </div>
+        : <div className='userProfileContainer'>
+          {openInfoModal ? <InfoModal
+            closeModal={setOpenInfoModal}
+            name={user.name}
+            lastname={user.lastname}
+            age={user.age}
+            email={user.email}
+            phone_number={user.phone_number}
+            location={user.location}
+            verified={user.verified}
+          /> : null}
+
+          {openBonoModal ? <BonoModal
+            closeBonoModal={setOpenBonoModal}
+            name={user.name}
+          /> : null}
+
+          <div className='infoSection'>
+            {/* <h2 className='userProfileText'>perfil de usuario</h2> */}
+            {user && (
+              <div className="userInfo">
+                <div className="Decorator"></div>
+                <div className="Info">
+                  {profileImg.length
+                    ? (
+                      image.map((image, i) => (
+                        <img
+                          key={i}
+                          src={profileImg}
+                          alt='imagen'
+                          className='userImage'
+                        />
+                      ))
+                    )
+                    : loading === true
+                      ? (
+                        <Loading color="primary" />
+                      )
+                      : (
+                        <img
+                          src='https://cdn-icons-png.flaticon.com/512/3135/3135715.png'
+                          alt='image default'
+                          className='userImage'
+                        />
+                      )}
+                  <h2>{user.name + ' ' + user.lastname}</h2>
+
 
                 </div>
+
               </div>
-             
+            )}
+            <div className='userButtonContainer'>
+              <button className='userButtons'
+                onClick={() => { setOpenInfoModal(!openInfoModal) }}>
+                Informacion
+              </button>
+
+              <button
+
+                className={`userButtons ${scroll && "scroll"}`}   >
+
+              Locales
+              </button>
+
+              <button
+                className='userButtons'
+                onClick={() => { setOpenBonoModal(!openBonoModal) }}>
+                Bonificaciones
+              </button>
+
+
             </div>
-           
-          )
-        })}
-      </div>
-      
-      <div className="Userprofile"></div>
-     
+          </div>
+
+
+
+
+          <div className='userAvatarContainer'>
+
+            <p>Cambiar Imagen De Perfil</p>
+            <input
+              type='file'
+              name='imagen'
+              accept='image/png,image/jpeg,image/jpg,image/gif'
+              onChange={handleChangeimages}
+              title='Cambiar Avatar'
+            ></input>
+          </div>
+
+
+
+
+          <div className='userReviews'>
+            <h1 className='reviewTittle'>Locales </h1>
+            {reviews && reviews.map((review) => {
+              return (
+                <div className='mainContainer'>
+                  <div key={review.id} className='reviewContainer'>
+                    <div className='reviewTitle'>
+                      <h3>Titulo: {review.title}</h3>
+                    </div>
+
+                    <div className='reviewInfoContainer'>
+
+                      <div className='reviewCalification'>
+                        <p>Comentario: {review.comment}</p>
+                        <p>Calificaciones:</p>
+                        <p>Food :{review.food}</p>
+                        <p>Service :{review.service}</p>
+                        <p>Environment :{review.environment}</p>
+                      </div>
+
+                      <figure className='imgContainer'>
+                        <img src={review.Image?.url} alt="" />
+                      </figure>
+
+
+
+                    </div>
+
+
+                    <div className='reviewButtons'>
+                      <button>Modificar</button>
+                      <button>Eliminar</button>
+
+                    </div>
+                  </div>
+
+                </div>
+
+              )
+            })}
+          </div>
+
+        </div>) 
+    )
+    
+  
+
     
    
-    </div>
-  );
-}
+  }
+
+
 
 export default Userprofile;
 
