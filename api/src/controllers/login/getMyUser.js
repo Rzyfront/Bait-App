d9e3f6340502630310b56d6bc374ab5fec182653
franco4457 <franco_forex@hotmail.com>
2023-04-29 12:04:47 -0300

    fix autoLog role

--- api/src/controllers/login/getMyUser.js
+++ api/src/controllers/login/getMyUser.js
@@ -7,6 +7,7 @@ module.exports = async (req, res) => {
     });
     if (!user) throw new Error('User not found');
     if (user.verified === 'suspended') throw new Error('Your account has been suspended');
+    if (user.role !== req.role) throw new Error('Your role has been modified, please login again');
     res.status(200).json({ user, success: true });
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
