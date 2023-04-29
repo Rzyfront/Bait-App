63d5eb13f07fbea4263dbeeb1c09eaa2f041151b
franco4457 <franco_forex@hotmail.com>
2023-04-29 12:01:09 -0300

    feat(/admin): ad user profile

--- api/src/controllers/users/getUserProfile.js
+++ api/src/controllers/users/getUserProfile.js
@@ -4,12 +4,19 @@ const {
 
 module.exports = async (req, res) => {
   try {
-    const { userId } = req;
-    const user = await User.findByPk(userId, {
+    const { userId } = req.params;
+    const ID = userId ?? req.userId;
+    const user = await User.findByPk(ID, {
       attributes: ['id', 'name', 'lastname', 'age', 'location', 'phone_number', 'email'],
       include: [
         { model: Image, attributes: ['url'] },
-        { model: Review, include: [{ model: Image, attributes: ['url'] }] },
+        {
+          model: Review,
+          include: [
+            { model: Image, attributes: ['url'], as: 'Image' },
+            { model: Image, attributes: ['url'], as: 'ticket' },
+          ],
+        },
         {
           model: Local,
           where: { verified: 'verified' },
