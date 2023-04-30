1a595866511d3d25719b88d8b6794d22963c764a
DianaQuintana <dcqy.95@gmail.com>
2023-04-30 16:28:54 -0500

    modification

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,8 +5,8 @@ import store from './redux/store/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
-// axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://bait-app.onrender.com';
+axios.defaults.baseURL = 'http://localhost:3001';
+//axios.defaults.baseURL = 'https://bait-app.onrender.com';
 axios.defaults.headers.common.Authorization = `Bearer ${window.localStorage.getItem('token')}`;
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
