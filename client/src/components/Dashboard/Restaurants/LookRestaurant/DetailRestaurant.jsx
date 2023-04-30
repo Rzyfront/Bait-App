b129a785def58eb1e89671623ff62682e8a38a85
juancamilo1478 <juancamilo1478@gmail.com>
2023-04-30 15:37:08 -0500

    saiz

--- client/src/components/Dashboard/Restaurants/LookRestaurant/DetailRestaurant.jsx
+++ client/src/components/Dashboard/Restaurants/LookRestaurant/DetailRestaurant.jsx
@@ -2,17 +2,24 @@ import { useDispatch, useSelector } from 'react-redux';
 import { DetailLocal } from '../../../../redux/actions/local';
 import { useEffect } from 'react';
 import './DetailRestaurant.css';
-import { getReviews } from '../../../../redux/actions/actions';
+import { getReviews, getUserProfile } from '../../../../redux/actions/actions';
 
 const DetailRestaurant = ({ id, handleDetail }) => {
   const dispatch = useDispatch();
-  const { detail, reviews } = useSelector((state) => state);
+  const { detail, reviews, userProfile } = useSelector((state) => state);
   useEffect(() => {
     dispatch(DetailLocal(id));
     dispatch(getReviews(id));
-    console.log(detail, reviews);
+
+    console.log(detail, reviews, userProfile);
   }, []);
-  console.log(detail, reviews);
+  useEffect(() => {
+    if (detail && detail.verified === 'verified') {
+      dispatch(getUserProfile(detail.UserId
+      ));
+    }
+  }, [detail]);
+  console.log(detail, reviews, userProfile);
   return <div className='detailRestaurantContainer'>
         <button onClick={handleDetail}>cerrar</button>
         blabla bla
