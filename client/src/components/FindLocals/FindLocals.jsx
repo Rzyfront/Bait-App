429523e641ce8b1bf539707474129e8a1737f2d9
edgarrios412 <edgarrios412@gmail.com>
2023-05-01 11:29:46 -0500

    Feat(./review) new model review form

--- client/src/components/FindLocals/FindLocals.jsx
+++ client/src/components/FindLocals/FindLocals.jsx
@@ -1,21 +1,32 @@
 import { useEffect, useState } from 'react';
 import style from './FindLocals.module.css';
 import Card from '../Card/Card';
-import { Link } from 'react-router-dom';
+import ReviewLocal from './ReviewLocal/ReviewLocal';
+import { toast } from 'react-toastify';
 
 const FindLocals = () => {
   const [nombre, setNombre] = useState();
+  const [selectedId, setSelectedId] = useState(null);
   const handleChange = (e) => setNombre(e.target.value);
   useEffect(() => {
     console.log('dispatch busqueda');
   }, [nombre]);
+
+  const sendReview = () => {
+    toast.success('Hemos enviado un email de verificacion', {
+      position: toast.POSITION.BOTTOM_CENTER
+    });
+    setSelectedId(null);
+  };
+
   return (
     <main className={style.main}>
+      { selectedId && <ReviewLocal sendReview={sendReview}/>}
       <h2 className={style.title}>¿Que lugar queres reseñar?</h2>
       <div className={style.inputContainer}>
       <input placeholder="Nombre" onChange={handleChange} className={style.input}/>
-      <div className={style.locals}>
-        <Link to="/reviewLocal"><Card id={1} Name="prueba" Rating={4} location="Buenos Aires" Images={[]} Price={1} lat={0} lng={0}/></Link>
+      <div className={style.locals} onClick={() => setSelectedId(1)}>
+        <Card id={1} Name="prueba" Rating={4} location="Buenos Aires" Images={[]} Price={1} lat={0} lng={0}/>
         <Card id={1} Name="prueba" Rating={4} location="Buenos Aires" Images={[]} Price={1} lat={0} lng={0}/>
         <Card id={1} Name="prueba" Rating={4} location="Buenos Aires" Images={[]} Price={1} lat={0} lng={0}/>
         <Card id={1} Name="prueba" Rating={4} location="Buenos Aires" Images={[]} Price={1} lat={0} lng={0}/>
