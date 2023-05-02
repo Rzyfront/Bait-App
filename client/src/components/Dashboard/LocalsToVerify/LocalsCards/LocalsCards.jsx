1be214c3d799197fe19e4e48b63bd06a07ea7263
franco4457 <franco_forex@hotmail.com>
2023-05-02 00:49:11 -0300

    feat(/localsToVerify): buttons

--- client/src/components/Dashboard/LocalsToVerify/LocalsCards/LocalsCards.jsx
+++ client/src/components/Dashboard/LocalsToVerify/LocalsCards/LocalsCards.jsx
@@ -1,12 +1,41 @@
 import axios from 'axios';
 import styles from './LocalsCards.module.css';
 import { useState } from 'react';
+import swal from 'sweetalert';
+import { useDispatch } from 'react-redux';
+import { getLocalsToVerify } from '../../../../redux/actions/LocalsAdmin';
 
 export default function LocalsCards ({ local }) {
+  const dispatch = useDispatch();
   const [documentLink, setDocumentLink] = useState();
   const handleDocument = async () => {
-    const { data } = await axios.get(`/locals/document/${local.id}`);
-    setDocumentLink(data.url);
+    try {
+      const { data } = await axios.get(`/locals/document/${local.id}`);
+      setDocumentLink(data.url);
+    } catch (error) {
+      console.log(error);
+      swal(error.response.data.message, { type: 'error' });
+    }
+  };
+  const handlerAssignLocal = async () => {
+    try {
+      await axios.put('/administrator/assignLocal', { userId: local.UserId, localId: local.id });
+      swal('Local asignado con éxito', { type: 'success' });
+      dispatch(getLocalsToVerify());
+    } catch (error) {
+      console.log(error);
+      swal(error.response.data.message, { type: 'error' });
+    }
+  };
+  const handlerDenyLocal = async () => {
+    try {
+      await axios.put('/administrator/denyLocal', { documentId: local.Document.id, localId: local.id });
+      swal('Local denegado con éxito', { type: 'success' });
+      dispatch(getLocalsToVerify());
+    } catch (error) {
+      console.log(error);
+      swal(error.response.data.message, { type: 'error' });
+    }
   };
   return (
     <>
@@ -15,8 +44,8 @@ export default function LocalsCards ({ local }) {
         <h2>{local?.User?.name}</h2>
         <button onClick={handleDocument} >Document</button>
         {documentLink && <a href={documentLink} target='_blank' rel="noreferrer" >Link al documento</a>}
-        <button>Verificar</button>
-        <button>Denegar</button>
+        <button onClick={handlerAssignLocal}>Asignar local</button>
+        <button onClick={handlerDenyLocal}>Denegar</button>
       </div>
     </>
   );
