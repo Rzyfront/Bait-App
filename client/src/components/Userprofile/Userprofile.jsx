77bb55ca791eae2910eb6f8e9b4e9966befff97c
Yixsi <82002852+Yixsi@users.noreply.github.com>
2023-05-03 18:22:10 -0500

    Revert "Change favicon and reviews user profile styled"

--- client/src/components/Userprofile/Userprofile.jsx
+++ client/src/components/Userprofile/Userprofile.jsx
@@ -1,31 +1,31 @@
 
-import { useDispatch, useSelector } from 'react-redux';
-import './Userprofile.css';
+import { useDispatch, useSelector } from "react-redux";
+import "./Userprofile.css";
 import {
   getReviews,
   getUserProfile,
   updateUser,
-  getUserLocals
-} from '../../redux/actions/actions';
-import { Rating as RatingStar } from '@smastrom/react-rating';
-import axios from 'axios';
-import { useParams } from 'react-router';
-import { useEffect, useState } from 'react';
-import { useUploadImage } from '../../hooks/useUploadImage';
-import { Loading } from '@nextui-org/react';
-import style from './UserProfile.module.css';
-import { FiUser, FiGift } from 'react-icons/fi';
-import { AiOutlineStar } from 'react-icons/ai';
-import { BiRestaurant, BiLogOutCircle } from 'react-icons/bi';
-import { useNavigate } from 'react-router-dom';
+  getUserLocals,
+} from "../../redux/actions/actions";
+import axios from "axios";
+import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { useUploadImage } from "../../hooks/useUploadImage";
+import { Loading } from "@nextui-org/react";
+import style from "./UserProfile.module.css";
+import { FiUser, FiGift } from "react-icons/fi"
+import { AiOutlineStar } from "react-icons/ai"
+import { BiRestaurant, BiLogOutCircle } from "react-icons/bi"
+import { useNavigate } from "react-router-dom";
 import { RiImageAddFill } from 'react-icons/ri';
 import swal from 'sweetalert'
 
-import UserLocals from './UserLocals';
+import UserLocals from "./UserLocals";
 
-const defaultImg = 'https://i.pinimg.com/474x/bc/df/1d/bcdf1dc5c5f2d1b6665f7f3ea8740ec7.jpg';
+const defaultImg = "https://www.shutterstock.com/image-vector/user-login-authenticate-icon-human-260nw-1365533969.jpg"
 
-function Userprofile () {
+
+function Userprofile() {
   const { image, loading, handleChangeimage } = useUploadImage();
 
   const [userData, setUserData] = useState({
@@ -37,10 +37,13 @@ function Userprofile () {
     image: { id: "", url: "" },
     location: "",
     id: ""
-  });
+
+  })
+
 
   const dispatch = useDispatch();
   const { userId } = useParams();
+
 
   const userProfile = useSelector((state) => state.userProfile);
   const obtainUserLocal = useSelector((state) => state.userDashLocals);
@@ -53,6 +56,7 @@ function Userprofile () {
   useEffect(() => {
     user && dispatch(getUserProfile(user.id));
   }, [user]);
+
 
   useEffect(() => {
     dispatch(getReviews(userId));
@@ -68,18 +72,19 @@ function Userprofile () {
       email: user.email,
       phone_number: user.phone_number,
       image: image[0],
-      location: user.location
-    });
+      location: user.location,
+    })
   }, [user, image]);
+
 
   useEffect(() => {
     setUserLocal(obtainUserLocal);
   }, [obtainUserLocal]);
 
   const handleChangeimages = (event) => {
-    alert('Recuerda guardar los cambios');
     handleChangeimage(event);
   };
+
 
   const handleDeleteReview = async (id) => {
 
@@ -128,39 +133,34 @@ function Userprofile () {
 
 
 
-  return (
-    <div className={style.profileContainer}>
-    <div className={style.navBar}>
-      <p className={style.title}>Mi perfil</p>
-      <ul className={style.ul}>
-          <li className={selectedId === 1 ? style.liSelected : style.li} onClick={() => setSelectedId(1)}><FiUser />  <span>Informacion</span></li>
-          <li className={selectedId === 2 ? style.liSelected : style.li} onClick={() => setSelectedId(2)}><AiOutlineStar /> <span>Reseñas</span></li>
-          {user?.role === 'owner' ? <li className={selectedId === 3 ? style.liSelected : style.li} onClick={() => setSelectedId(3)}><BiRestaurant /> <span>Locales</span></li> : null}
-          <li className={selectedId === 4 ? style.liSelected : style.li} onClick={() => setSelectedId(4)}><FiGift /> <span>Bonificaciones</span></li>
-          <li className={style.li} onClick={handleInicio}><BiLogOutCircle /> <span>Salir</span></li>
-       </ul>
-    </div>
-    <div className={style.menu}>
-      {selectedId === 1 && <div className={style.infoMenu}>
-        <div className={style.resumeInfo}>
-                <input
-                 type="file"
-                  name="file"
-                   className={style.inputFile}
-                  onChange={ handleChangeimages } />
-            <img src={user?.Image ? user?.Image?.url : defaultImg} className={style.imgProfile} name="Image"/>
-          <div>
-            <p className={style.name}>{user && user.name}</p>
-            <p className={style.email}>{user && user.email}</p>
-          </div>
+    return (
+      <div className={style.profileContainer}>
+        <div className={style.navBar}>
+          <p className={style.title}>Mi perfil</p>
+          <ul className={style.ul}>
+            <li className={selectedId == 1 ? style.liSelected : style.li} onClick={() => setSelectedId(1)}><FiUser />  <span>Informacion</span></li>
+            <li className={selectedId == 2 ? style.liSelected : style.li} onClick={() => setSelectedId(2)}><AiOutlineStar /> <span>Reseñas</span></li>
+            {user?.role === "owner" ? <li className={selectedId == 3 ? style.liSelected : style.li} onClick={() => setSelectedId(3)}><BiRestaurant /> <span>Locales</span></li> : null}
+            <li className={selectedId == 4 ? style.liSelected : style.li} onClick={() => setSelectedId(4)}><FiGift /> <span>Bonificaciones</span></li>
+            <li className={style.li} onClick={handleInicio}><BiLogOutCircle /> Salir</li>
+          </ul>
         </div>
-        <div className={style.form}>
-        <div className={style.formLeft}>
-        <div className={style.input}>
-                <input onChange={handleChange} name="name" className={style.inputForm} value={userData.name} />
-                <label htmlFor="name" className={style.placeholder}>
-                  Nombre
-                </label>
+        <div className={style.menu}>
+          {selectedId == 1 && <div className={style.infoMenu}>
+            <div className={style.resumeInfo}>
+              <img src={user?.Image ? user?.Image?.url : defaultImg} className={style.imgProfile} name="Image" />
+              <div>
+                <p className={style.name}>{user && user.name}</p>
+                <p className={style.email}>{user && user.email}</p>
+              </div>
+            </div>
+            <div className={style.form}>
+              <div className={style.formLeft}>
+                <div className={style.input}>
+                  <input onChange={handleChange} name="name" className={style.inputForm} value={userData.name} />
+                  <label htmlFor="name" className={style.placeholder}>
+                    Nombre
+                  </label>
 
                 </div>
                 <div className={style.input}>
@@ -176,79 +176,94 @@ function Userprofile () {
                   </label>
                 </div>
               </div>
-              <div className={style.input}>
-                <input name="phone_number" className={style.inputForm} value={userData.phone_number} onChange={handleChange} />
-                <label htmlFor="phone_number" className={style.placeholder}>
-                  Telefono
-                </label>
-              </div>
-        </div>
-        </div>
-        <button onClick={handleSave} className={style.saveChanges}>Guardar</button>
-      </div>}
-      {selectedId === 2 && <div className={style.myLocals}>
-        <p className={style.titleLocal}>Ultimas reseñas</p>
-        <div className={style.reviews}>
-          <div className={style.reviewContainer}>
-            <div style={{ display: 'flex' }}>
-          <h4 className={style.titleReview}>Mala atencion</h4><RatingStar
-                  className={style.ratingStar}
-                  name='Rating'
-                  style={{ maxWidth: 100, marginLeft: '20px' }}
-                  value={4}
-                  readOnly
-                />
+              <div className={style.formRight}>
+                <div className={style.input}>
+                  <input onChange={handleChange} name="lastname" className={style.inputForm} value={userData.lastname} />
+                  <label htmlFor="lastname" className={style.placeholder}>
+                    Apellido
+                  </label>
                 </div>
-              <p className={style.commentReview}>Se demoraron como 4 horas para traerme el perdido y cuando llego estaba frio que gonorrea parce</p>
-              <div className={style.detailReview}>
-                <p className={style.dateReview}>04/12/2023</p>
-              </div>
-              <div style={{ textAlign: 'center', marginTop: '20px' }}>
-                <button className={style.deleteReview}>Eliminar</button>
-              </div>
-          </div>
-          { userProfile?.Reviews.map((rev, i) => {
-            return (<div key={i} className={style.reviewContainer}>
-            <div style={{ display: 'flex' }}>
-          <h4 className={style.titleReview}>{rev.title}</h4><RatingStar
-                  className={style.ratingStar}
-                  name='Rating'
-                  style={{ maxWidth: 100, marginLeft: '20px' }}
-                  value={4}
-                  readOnly
-                />
+                <div className={style.input}>
+                  <input name="age" className={style.inputForm} value={userData.age} onChange={handleChange} />
+                  <label htmlFor="age" className={style.placeholder}>
+                    Edad
+                  </label>
                 </div>
-              <p className={style.commentReview}>{rev.comment}</p>
-              <div className={style.detailReview}>
-                <p className={style.dateReview}>{rev.updatedAt}</p>
+                <div className={style.input}>
+                  <input
+                    type="file"
+                    name="file"
+                    className={style.inputForm}
+                    onChange={handleChangeimages} />
+                  <label htmlFor="titulo" className={style.placeholder}>
+                    Cambiar Imagen
+                  </label>
+                  {image.length
+                    ? (
+                      image.map((image, i) => (
+                        <img
+                          key={i}
+                          src={image.url}
+                          alt='imagen'
+                          className='LocalesImage'
+                        />
+                      ))
+                    )
+                    : loading === true
+                      ? (
+                        <Loading color="primary" />
+                      )
+                      : (
+
+                        <RiImageAddFill className='LocalesImage' />
+                      )}
+                </div>
               </div>
-              <div style={{ textAlign: 'center', marginTop: '20px' }}>
-                <button className={style.deleteReview}>Eliminar</button>
-              </div>
-            </div>);
-          })}
-          </div>
+            </div>
+            <button onClick={handleSave} className={style.saveChanges}>Guardar</button>
           </div>}
-      {selectedId === 3 && <div className={style.myLocals}>
-        <p className={style.titleLocal}>Mis locales</p>
-        <div className={style.localContainer}>
-          <div className={style.local}></div>
-          <div className={style.local}></div>
-          <div className={style.local}></div>
-          {/* <div className={style.local}></div> */}
-          {/* <div className={style.local}></div> */}
+          {selectedId == 2 && <div className={style.myLocals}>
+            <p className={style.titleLocal}>Ultimas reseñas</p>
+            {userProfile?.Reviews.map((rev) => {
+              return (<div key={rev.id}>
+                <h4>{rev.title}</h4>
+                <p>{rev.comment}</p>
+                <div>
+                  <p>Calificaciones</p>
+                  <p>Ambiente :{rev.environment}</p>
+                  <p>Comida :{rev.food}</p>
+                  <p>Calida-Precio : {rev.qaPrice}</p>
+                  <p>Servicio{rev.service}</p>
+                  <p>Estado:{rev.verified}</p>
+                  <p>Fecha de Creacion : {rev.updatedAt}</p>
+
+                </div>
+                <div >
+                  <button id={rev.id} onClick={() => { handleDeleteReview(rev.id) }}>Eliminar Review</button>
+                </div>
+              </div>)
+            })}
+          </div>}
+          {selectedId == 3 && <div className={style.myLocals}>
+            <p className={style.titleLocal}>Mis locales</p>
+            <div className={style.localContainer}>
+              <div className={style.local}></div>
+              <div className={style.local}></div>
+              <div className={style.local}></div>
+              {/* <div className={style.local}></div> */}
+              {/* <div className={style.local}></div> */}
+            </div>
+          </div>}
+          {selectedId == 4 && <div className={style.giftMenu}>
+            <p className={style.titleLocal}>Bonificaciones</p>
+            <img src="https://cdn-icons-png.flaticon.com/512/5957/5957125.png" className={style.imgGift} />
+            <p className={style.titleGift}>Lamentamos informarte que las recompensas no estan activas</p>
+          </div>
+          }
         </div>
-      </div>}
-      {selectedId === 4 && <div className={style.giftMenu}>
-        <p className={style.titleLocal}>Bonificaciones</p>
-        <img src="https://cdn-icons-png.flaticon.com/512/5957/5957125.png" className={style.imgGift}/>
-        <p className={style.titleGift}>Lamentamos informarte que las recompensas no estan activas</p>
       </div>
-      }
-    </div>
-    </div>
 
-  );
-}
+    );
+  } 
+  export default Userprofile;
 
-export default Userprofile;
