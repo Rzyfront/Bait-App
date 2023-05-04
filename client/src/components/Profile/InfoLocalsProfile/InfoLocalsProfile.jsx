03cb0ea355403203869a4b8fccd1bf005a50571a
rzyfront <rzyfront@gmail.com>
2023-05-04 04:15:03 -0500

    fix styles/resposives

--- client/src/components/Profile/InfoLocalsProfile/InfoLocalsProfile.jsx
+++ client/src/components/Profile/InfoLocalsProfile/InfoLocalsProfile.jsx
@@ -2,30 +2,56 @@ import './InfoLocalsProfile.css';
 import { GoLocation } from 'react-icons/go';
 import { Rating as RatingStar, ThinStar } from '@smastrom/react-rating';
 function InfoLocalsProfile ({ detail, showClaimLocal }) {
-  const { name, location, rating, avgEnvironment, avgFood, avgQaPrice, avgService, Characteristic } = detail;// eslint-disable-line
+  const { name, location, rating, avgEnvironment, avgFood, avgQaPrice, avgService, Characteristic,specialty } = detail;// eslint-disable-line
   const myStyles = {
     itemShapes: ThinStar,
     activeFillColor: '#343434',
     inactiveFillColor: '#3434343B'
   };
+
+  const properties = {
+    big_group: 'Grupo grande',
+    family_style: 'Familiar',
+    live_music: 'Música',
+    outdoor_seating: 'Aire libre',
+    parking_lot: 'Estacionamiento',
+    pet_friendly: 'Acepta mascotas',
+    romantic: 'Romántico',
+    table_service: 'Servicio de mesa',
+    wifi: 'Wifi',
+    work_friendly: 'Para trabajar'
+  };
+
+  const trueProperties = [];
+
+  if (Characteristic) {
+    for (const [property, value] of Object.entries(Characteristic)) {
+      if (value && properties[property]) {
+        trueProperties.push(properties[property]);
+      }
+    }
+  }
+  console.log(detail);
   return (
     <div className='InfoLocalsProfile-Compontent'>
         <div className='Info-left'>
         <div className='Tags-Info-Group'>
-          <div className='Tag-Type-Eat' title='Tipo de Comida'>
-            <h5 className='Tag-Text'>Argentina</h5>
-        </div>
+         {specialty && <div className='Tag-Type-Eat' title='Tipo de Comida'>
+            <h5 className='Tag-Text'>{specialty}</h5>
+        </div>}
         <div className='Tag-Type-Local' title='Tipo de Restaurant'>
             <h5 className='Tag-Text'>Colonial</h5>
         </div>
         </div>
         <h2 className='Info-Local-Name'>{name}</h2>
-        <h4 className='Info-Local-Location'><GoLocation className='locationico' />  {location}</h4>
+        <h4 className='Info-Local-Location'><GoLocation className='locationico'/>  {location}</h4>
         <h4 className='Info-Rating' title={`Rating promedio ${rating || 0}`}>Calificación: <RatingStar readOnly style={{ maxWidth: 150 }} value={rating || 0} className='Stars-Cards' itemStyles={myStyles}/></h4>
         <div className='Info-Caracteristic-Group'>
-            <h5 className='Info-C-Title'>Características:</h5>
+           {trueProperties.length ? <h5 className='Info-C-Title'>Características:</h5> : ''}
             {/* Mapear characteristias y renderizar cada una true en un div */}
-            <div className='Info-C-Tag'>Sin Características</div>
+            {trueProperties?.map((p, i) => {
+              return <div key={i} className='Info-C-Tag'>{p}</div>;
+            })}
         </div>
         </div>
         <div className='Info-rigth'>
@@ -34,10 +60,10 @@ function InfoLocalsProfile ({ detail, showClaimLocal }) {
               {/* <h4 className='Info-rinth-Title'>Categorías calificadas</h4> */}
                         </div>
             <div className='Rating-C-Group-Container'>
-                <h5 className='Rating-C-Group'>Ambiente: <RatingStar readOnly style={{ maxWidth: 100 }} value={rating || 0} className='Stars-Cards' itemStyles={myStyles}/></h5>
-                <h5 className='Rating-C-Group'>Comida: <RatingStar readOnly style={{ maxWidth: 100 }} value={rating || 0} className='Stars-Cards' itemStyles={myStyles}/></h5>
-                <h5 className='Rating-C-Group'>Calidad-Precio: <RatingStar readOnly style={{ maxWidth: 100 }} value={rating || 0} className='Stars-Cards' itemStyles={myStyles}/></h5>
-                <h5 className='Rating-C-Group'>Servicio: <RatingStar readOnly style={{ maxWidth: 100 }} value={rating || 0} className='Stars-Cards' itemStyles={myStyles}/></h5>
+                <h5 className='Rating-C-Group'>Ambiente: <RatingStar readOnly style={{ maxWidth: 100 }} value={avgEnvironment || 0} className='Stars-Cards' itemStyles={myStyles}/></h5>
+                <h5 className='Rating-C-Group'>Comida: <RatingStar readOnly style={{ maxWidth: 100 }} value={avgFood || 0} className='Stars-Cards' itemStyles={myStyles}/></h5>
+                <h5 className='Rating-C-Group'>Calidad-Precio: <RatingStar readOnly style={{ maxWidth: 100 }} value={avgQaPrice || 0} className='Stars-Cards' itemStyles={myStyles}/></h5>
+                <h5 className='Rating-C-Group'>Servicio: <RatingStar readOnly style={{ maxWidth: 100 }} value={avgService || 0} className='Stars-Cards' itemStyles={myStyles}/></h5>
             </div>
         </div>
     </div>
