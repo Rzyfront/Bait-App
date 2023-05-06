59cb2af411b464586569901c8e77cc708f2d1490
franco4457 <franco_forex@hotmail.com>
2023-05-06 15:14:10 -0300

    update description docs

--- api/src/swagger.js
+++ api/src/swagger.js
@@ -9,7 +9,7 @@ const options = {
     info: {
       title: 'Bait-API',
       version: '1.0.0',
-      description: 'Esta es una RESTFUL API desarrolada para bait-app en ella se pueden encontrar todos lo endpoint utilizados por el frontend, hay algunos endpoints que no estan documentados deibido a la falta de tiempo y por que de momento no son utilizados por el frontend.Att:Franco Gutierrez(franco4457)',
+      description: 'Esta es una RESTFUL API desarrolada para bait-app en ella se pueden encontrar todos lo endpoint utilizados por el frontend, tanto para la creación, actualización y verificación de locales y reseñas, la creacion de usuarios y gestion de los mismo.Las respuestas de la API inicialmente estaban en ingles pero por causas de fuerza mayor fueron traducidas al español, por eso algunas partes de la documentacion tiene respuestas en ingles pero la respuesta enpoint actualmente es en español.Hay algunos endpoints que no estan documentados deibido a la falta de tiempo y por que de momento no son utilizados por el frontend.Att:Franco Gutierrez(franco4457)',
     },
     basePath: '/',
   },
